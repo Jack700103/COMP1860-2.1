1c03c003959ca8c84cb9d8e4d94d2217135b9c9c
Jack700103 <wqzl0852@leeds.ac.uk>
2026-03-04 09:46:44 +0000

    data

--- SumEvenIntegers.asm
+++ SumEvenIntegers.asm
@@ -21,51 +21,41 @@ D;JEQ
 // n > 0 的情况，开始计算
 @R0
 D=M
-@COUNT
-M=D             // COUNT = n (循环次数)
+@COUNTER
+M=D             // COUNTER = n (剩余要加的项数)
 @R1
 M=0             // R1 = 0 (结果累加器)
-@CURRENT_I
-M=0             // CURRENT_I = 0 (当前 i 值)
+@CURRENT_EVEN
+M=0             // CURRENT_EVEN = 0 (当前要加的偶数)
 
 (CALC_LOOP)
-@CURRENT_I
+@COUNTER
 D=M
-@COUNT
-D=D-M           // 比较当前 i 和 n
 @CALC_DONE
-D;JGE           // 如果 i >= n，结束循环
+D;JLE           // 当计数器 <= 0 时结束
 
-// 计算 2*i
-@CURRENT_I
-D=M
-@TEMP_DOUBLE
-M=D
-@TEMP_DOUBLE
-M=M+D           // TEMP_DOUBLE = 2 * CURRENT_I
-
-// 计算 R1 + 2*i 的结果，检查溢出
+// 计算 R1 + CURRENT_EVEN 的结果，检查溢出
 @R1
 D=M
 @TEMP_SUM
 M=D             // 临时保存当前和
 
-@TEMP_DOUBLE
+@CURRENT_EVEN
 D=M
 @TEMP_SUM
-D=D+M           // 计算 R1 + 2*i
+D=D+M           // 计算 R1 + CURRENT_EVEN
 
 // 溢出检查：当两个非负数相加结果为负数时发生溢出
-// R1 (当前和) 应该非负，TEMP_DOUBLE (2*i) 也应该非负
+// 由于我们从0开始加正偶数，R1和CURRENT_EVEN都应该是非负的
 @R1
 D=M
 @CHECK_POS_ADD
-D;JLT           // 如果R1是负数（可能由于之前溢出），跳过检查
+D;JLT           // 如果R1已经是负数（可能由于溢出），则跳过检查
 
-@TEMP_DOUBLE
+@CURRENT_EVEN
 D=M
 @CHECK_POS_ADD
-D;JLT           // 如果2*i是负数（不太可能，但检查一下）
+D;JLT           // 如果CURRENT_EVEN是负数（不应该），也跳过检查
 
 // 两个操作数都非负，检查结果是否为负（溢出）
 @TEMP_SUM
@@ -80,10 +70,15 @@ D=M
 @R1
 M=D
 
-// i++
-@CURRENT_I
+// 更新下一个偶数 (CURRENT_EVEN += 2)
+@CURRENT_EVEN
+M=M+1
+@CURRENT_EVEN
 M=M+1
 
+// 计数器减1
+@COUNTER
+M=M-1
 @CALC_LOOP
 0;JMP
 
@@ -99,7 +94,7 @@ M=-1            // n < 0, 设置 R1 = -1
 
 (CHECK_ZERO)
 @R1
-M=0             // n = 0, 设置 R1 = 0 (0+2*0=0)
+M=0             // n = 0, 设置 R1 = 0 (空和为0)
 @END
 0;JMP
 
