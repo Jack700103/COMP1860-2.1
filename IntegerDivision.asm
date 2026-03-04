554b6def9e28a0604c756fba5f451216c520180a
Jack700103 <wqzl0852@leeds.ac.uk>
2026-03-04 03:29:20 +0000

    data

--- IntegerDivision.asm
+++ IntegerDivision.asm
@@ -1,109 +1,23 @@
-    @R1
-    D=M
-    @INVALID
-    D;JEQ
-
-    @R4
-    M=0
-
-    @R0
-    D=M
-    @R5
-    M=D
-    @CHECK_X_SIGN
-    D;JGE
-    @R5
-    M=-M
-    (CHECK_X_SIGN)
-
-    @R1
-    D=M
-    @R6
-    M=D
-    @CHECK_Y_SIGN
-    D;JGE
-    @R6
-    M=-M
-    (CHECK_Y_SIGN)
-
-    @R2
-    M=0
-
-    (DIV_LOOP)
-    @R5
-    D=M
-    @R6
-    D=D-M
-    @APPLY_SIGNS
-    D;LT
-
-    @R5
-    M=D
-
-    @R2
-    M=M+1
-
-    @DIV_LOOP
-    0;JMP
-
-    (APPLY_SIGNS)
-    @R0
-    D=M
-    @REM_POSITIVE
-    D;JGE
-
-    @R5
-    D=-M
-    @STORE_REM
-    0;JMP
-
-    (REM_POSITIVE)
-    @R5
-    D=M
-
-    (STORE_REM)
-    @R3
-    M=D
-
-    @R0
-    D=M
-    @X_POS
-    D;JGE
-    @X_NEG
-    0;JMP
-
-    (X_POS)
-    @R1
-    D=M
-    @FINISH
-    D;JLT
-    @FINISH
-    0;JMP
-
-    (X_NEG)
-    @R1
-    D=M
-    @NEGATE_QUOTIENT
-    D;JGE
-    @FINISH
-    0;JMP
-
-    (NEGATE_QUOTIENT)
-    @R2
-    M=-M
-
-    (FINISH)
-    @END
-    0;JMP
-
-    (INVALID)
-    @R4
-    M=1
-    @R2
-    M=0
-    @R3
-    M=0
-
-    (END)
-    @END
-    0;JMP
+@R0
+D=M
+@R1
+D=D/M
+@R2
+M=D
+D=D*M
+@R0
+D=M-D
+@R3
+M=D
+@R1
+D=M
+@CHECK_DIV_BY_ZERO
+D;JEQ
+@R4
+M=0
+@END
+0;JMP
+(CHECK_DIV_BY_ZERO)
+@R4
+M=1
+(END)
