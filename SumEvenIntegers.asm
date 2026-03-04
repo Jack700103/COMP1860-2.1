5d27bbddfb6509cbacbe6d52a058c6b6db750d7f
Jack700103 <wqzl0852@leeds.ac.uk>
2026-03-04 07:58:27 +0000

    data

--- SumEvenIntegers.asm
+++ SumEvenIntegers.asm
@@ -1,70 +1,75 @@
-    @R0
-    D=M
-    @NEGATIVE
-    D;JLT
+@R0
+D=M
+@NEGATIVE_CASE
+D;JLT          
 
-    @R1
-    M=0        
-    @i
-    M=0        
+@R0
+D=M
+@R3
+M=D+1         
 
-(LOOP)
-    @i
-    D=M
-    @R0
-    D=D-M     
-    @END
-    D;JGT
+@R1
+M=0
+@R0
+D=M
+@R4
+M=D            
+@MULTIPLY_LOOP
+0;JMP         
 
-    @i
-    D=M
-    @2
-    D=D+A     
-    @R1
-    M=D+M      
+(MULTIPLY_LOOP)
+@R4
+D=M
+@OVERFLOW_CHECK_FINAL
+D;JLE          
 
-    @R1
-    D=M
-    @PREV
-    M=D        
-    @i
-    D=M
-    @2
-    D=D+A
-    @R1
-    M=M-D      
-    @PREV
-    D=M
-    @R1
-    M=D       
-    @R1
-    D=M
-    @PREVSUM
-    M=D        
-    @R1
-    D=M
-    @PREVSUM
-    D=D-M      
-    @OVERFLOW
-    D;JLT
+@R3
+D=M
+@SIGN_A
+M=D           
+@R1
+D=M
+@SIGN_RES
+M=D           
 
-    @i
-    M=M+1
-    @LOOP
-    0;JMP
+@R3
+D=M
+@R1
+M=D+M          
 
-(NEGATIVE)
-    @R1
-    M=-1
-    @END
-    0;JMP
+@SIGN_A
+D=M
+@SIGN_RES
+D=D&M         
+@SIGN_SAME
+D;JLT          
+@OVERFLOW_CASE 
+0;JMP
 
-(OVERFLOW)
-    @R1
-    M=-2
-    @END
-    0;JMP
+(SIGN_SAME)
+@R4
+M=M-1          
+@MULTIPLY_LOOP
+0;JMP         
+
+(NEGATIVE_CASE)
+@R1
+M=-1          
+@END
+0;JMP
+
+(OVERFLOW_CASE)
+@R1
+M=-2           
+@END
+0;JMP
+
+(OVERFLOW_CHECK_FINAL)
+@R1
+D=M
+@END
+0;JMP
 
 (END)
-    @END
-    0;JMP
+@END
+0;JMP
